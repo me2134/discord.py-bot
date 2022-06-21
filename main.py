7c390a771ab3cf88595e029d44dde65175cba574
Dominic <68821288+me2134@users.noreply.github.com>
2022-06-21 15:15:11 -0400

    Added some comments

--- main.py
+++ main.py
@@ -2,13 +2,12 @@ import discord
 import json
 from discord.ext import commands
 
-with open('info.json') as f:
+with open('info.json') as f: # Going into the json file with the toke, and setting the variable "token" to the bots token
     data = json.load(f)
 token = data["token"]
 
 
-intents = discord.Intents.all()
-intents.members = True
+intents = discord.Intents.all() # Enable all intents
 
 bot = commands.Bot(command_prefix='!', intents=intents)
 
@@ -18,10 +17,11 @@ async def on_ready():
     await bot.change_presence(status=discord.Status.idle)
 
 @bot.command()
-async def ping(ctx):
+async def ping(ctx): # Finds the latency of the bot
     await ctx.send(f'{bot.latency}ms')
+    
 @bot.command
-async def users(ctx):
+async def users(ctx): # Finds the number of users in the guild
   await ctx.send(f'{ctx.guild.name} has {ctx.guild.member_count} users')
 
 bot.run(token)
