a0d32cf59306aa886a51f879de1613f7a2da1580
Dominic <68821288+me2134@users.noreply.github.com>
2022-06-21 20:07:21 +0000

    Fixed Error upon using "users" command

--- main.py
+++ main.py
@@ -8,19 +8,20 @@ token = data["token"]
 
 
 intents = discord.Intents.all() # Enable all intents
+intents.members = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user}')
-    await bot.change_presence(status=discord.Status.idle)
+    await bot.change_presence(status=discord.Status.idle, activity =discord.Game("game"))
 
 @bot.command()
 async def ping(ctx): # Finds the latency of the bot
     await ctx.send(f'{bot.latency}ms')
     
-@bot.command
+@bot.command()
 async def users(ctx): # Finds the number of users in the guild
   await ctx.send(f'{ctx.guild.name} has {ctx.guild.member_count} users')
 
