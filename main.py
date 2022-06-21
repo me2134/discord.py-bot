91af860f3be72a0f369bf682ad5fa565048f8129
Dominic <68821288+me2134@users.noreply.github.com>
2022-06-21 15:12:21 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
- #988598060538015815
+
 import discord
 import json
 from discord.ext import commands
@@ -15,7 +15,7 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.event
 async def on_ready():
-    print(f'We have logged in as {bot.user}')
+    print(f'Logged in as {bot.user}')
     await bot.change_presence(status=discord.Status.idle)
 
 @bot.command()
